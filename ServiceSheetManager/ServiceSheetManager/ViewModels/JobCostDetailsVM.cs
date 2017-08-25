fa50f77b5c312d86629b5e73a114fcdb23c138d3
Richard Town <richard.town@gmail.com>
2017-08-25 20:23:19 +0100

    Updated layout on job cost screen

--- ServiceSheetManager/ServiceSheetManager/ViewModels/JobCostDetailsVM.cs
+++ ServiceSheetManager/ServiceSheetManager/ViewModels/JobCostDetailsVM.cs
@@ -35,14 +35,31 @@ namespace ServiceSheetManager.ViewModels
         [DataType(DataType.Date)]
         public DateTime ActiveJobDateTo { get; set; }
 
+        [Display(Name = "Number Engineers")]
         public int NumberEngineers { get; set; }
+
+        [Display(Name = "Total Days")]
         public int TotalDays { get; set; }
+
+        [Display(Name = "Total Onsite Hours")]
         public double TotalHoursOnsite { get; set; }
+
+        [Display(Name = "Total Travel Time")]
         public double TotalTravelTime { get; set; }
+
+        [Display(Name = "Total Daily Allowances")]
         public int TotalDailyAllowances { get; set; }
+
+        [Display(Name = "Total Overnight Allowances")]
         public int TotalOvernightAllowances { get; set; }
+
+        [Display(Name = "Total Mileage")]
         public int TotalMileage { get; set; }
+
+        [Display(Name = "Total Standard Hours")]
         public double StandardHours { get; set; }
+
+        [Display(Name = "Total Overtime Hours")]
         public double OvertimeHours { get; set; }
     }
 }
