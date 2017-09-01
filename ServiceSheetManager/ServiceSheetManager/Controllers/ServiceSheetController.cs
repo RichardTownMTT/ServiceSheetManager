cc43481d3b5420f7ab87982dd544677f2d881566
Richard Town <richard.town@gmail.com>
2017-09-01 20:50:47 +0100

    Created service sheet index page and display

---
+++ ServiceSheetManager/ServiceSheetManager/Controllers/ServiceSheetController.cs
@@ -0,0 +1,38 @@
+﻿using ServiceSheetManager.Models;
+using ServiceSheetManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServiceSheetManager.Controllers
+{
+    public class ServiceSheetController : Controller
+    {
+        private ServiceSheetsEntities db = new ServiceSheetsEntities();
+
+        // GET: ServiceSheet
+        public async Task<ActionResult> Index()
+        {
+            ServiceSheetIndexVM vm = new ServiceSheetIndexVM();
+
+            var serviceSheets = await db.ServiceSheets.OrderByDescending(s => s.SubmissionNumber).Take(5).ToListAsync();
+
+            vm.ServiceSheets = serviceSheets;
+
+            return View(vm);
+        }
+
+        //Display
+        public async Task<ActionResult> Display([Bind] int SubmissionNumber)
+        {
+            ServiceSheet sheet = await db.ServiceSheets.Where(s => s.SubmissionNumber.Equals(SubmissionNumber))
+                .Include(s => s.ServiceDays).FirstOrDefaultAsync();
+
+            return View(sheet);
+        }
+    }
+}
---
+++ ServiceSheetManager/ServiceSheetManager/ViewModels/ServiceSheetIndexVM.cs
@@ -0,0 +1,18 @@
+﻿using ServiceSheetManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceSheetManager.ViewModels
+{
+    public class ServiceSheetIndexVM
+    {
+        public List<ServiceSheet> ServiceSheets { get; set; }
+
+        public ServiceSheetIndexVM()
+        {
+            ServiceSheets = new List<ServiceSheet>();
+        }
+    }
+}
